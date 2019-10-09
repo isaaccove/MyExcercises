4db0d04c2d40bc4804f68b2c877e4aa86c51e2b3
ISAAC COVE <isaaccove@gmail.com>
2019-10-09 08:45:26 +0100

    My first commit

---
+++ AccountTest.java
@@ -0,0 +1,53 @@
+//import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class AccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		name = JOptionPane.showInputDialog("Enter New Name");
+		
+		String credit;
+		credit = JOptionPane.showInputDialog("Enter Credit Amount");
+		
+		//Scanner input = new Scanner(System.in);
+		
+Account isaacAccount = new Account();
+Account kemiAccount = new Account();
+Account kunleAccount = new Account();
+Account creditAccount = new Account();
+
+//System.out.println("Enter new Name:");
+ //String name = input.nextLine();
+ 
+ //System.out.println("Enter credit amount:");
+ //double value = input.nextDouble();
+JOptionPane.showMessageDialog(null, credit);
+ //System.out.printf("Your balance is: %.0f", creditAccount.credit(value));
+ //System.out.printf("Your balance is: %.0f", creditAccount.deduct(value));
+ 
+ 
+ //double money = input.nextDouble();
+ //double result = money- (0.05 * money);
+ 
+ //isaacAccount.setName(name);
+ //kemiAccount.setName(name);
+ //kunleAccount.setName(name);
+ //creditAccount.setCredit(money);
+ 
+ //System.out.printf("Your balance is %.0f%n\n", result);
+ //System.out.printf("Isaac account name is: %s \n", isaacAccount.getName());
+ //System.out.printf("Kunle account name is: %s \n", kunleAccount.getName());
+ //System.out.printf("Kemi account name is: %s \n", kemiAccount.getName());
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+	}
+
+}
